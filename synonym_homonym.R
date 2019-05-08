5a340c4e1e36e18e2bfaa58eab888c4081b15234
Unknown <besio1@bfh.ch>
2019-05-08 17:48:19 +0200

    add vierfeldertafel and homonym synonym description

---
+++ synonym_homonym.R
@@ -0,0 +1,16 @@
+# false non-match = synonym
+# Bsp: Handy und Mobiltelefon
+# Bsp. für Person: Martin und Mäddä (gleiche Person ist gemeint)
+# Haben keinen Match, sind aber trotzdem Sinnverwandt (gleiches Objekt gemeint)
+# c in Vierfeldertafel
+
+#TODO CODE
+
+
+# false match = homonym
+# Bsp: Bank (Sitzbank) und Bank (Geldinstitut)
+# Bsp. für Person: Jan und Jan (zwei unterschiedliche Personen)
+# Haben einen Match, da beide gleich sind, jedoch zwei unterschiedliche Objekte
+# b in Vierfeldertafel
+
+#TODO CODE
